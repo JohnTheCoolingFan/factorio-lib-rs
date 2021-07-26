86efd61cad2cb1893b474706dfc48b2352c547a8
JohnTheCoolingFan <ivan8215145640@gmail.com>
2021-07-26 10:04:23 +0300

    Derive module

---
+++ factorio-lib-rs-derive/src/lib.rs
@@ -0,0 +1,25 @@
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn;
+
+#[proc_macro_derive(ModSetting)]
+pub fn mod_setting_macro_derive(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+    impl_mod_setting_macro(&ast)
+}
+
+fn impl_mod_setting_macro(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl ModSetting for #name {
+            fn localised_name(&self) -> Option<LocalisedString> { self.localised_name.clone() }
+            fn localised_description(&self) -> Option<LocalisedString> { self.localised_description.clone() }
+            fn order(&self) -> Option<String> { self.order.clone() }
+            fn hidden(&self) -> Option<bool> { self.hidden }
+            fn setting_type(&self) -> ModSettingType { self.setting_type.clone() }
+        }
+    };
+    gen.into()
+}
