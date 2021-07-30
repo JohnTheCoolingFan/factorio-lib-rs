34f446155f6882d82739d61f51b6b7aa4c904491
JohnTheCoolingFan <ivan8215145640@gmail.com>
2021-07-30 22:33:40 +0300

    TODOs

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,8 @@ pub mod concepts;
 pub mod prototypes;
 pub mod mod_structs;
 pub mod types;
+
+// I guess this is a good place for todos
+
+// TODO: FromLua for prototypes
+// TODO: rest of the prototypes
