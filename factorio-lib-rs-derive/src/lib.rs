0a81fe40147d453aaad8013baa8c3fc35b4e7372
JohnTheCoolingFan <ivan8215145640@gmail.com>
2021-08-01 16:41:54 +0300

    PrototypeBAse derive macro

--- factorio-lib-rs-derive/src/lib.rs
+++ factorio-lib-rs-derive/src/lib.rs
@@ -10,6 +10,12 @@ pub fn mod_setting_macro_derive(input: TokenStream) -> TokenStream {
     impl_mod_setting_macro(&ast)
 }
 
+#[proc_macro_derive(PrototypeBase)]
+pub fn prototype_base_macro_derive(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+    impl_prototype_base_macro(&ast)
+}
+
 fn impl_mod_setting_macro(ast: &syn::DeriveInput) -> TokenStream {
     let name = &ast.ident;
     let gen = quote! {
@@ -19,6 +25,18 @@ fn impl_mod_setting_macro(ast: &syn::DeriveInput) -> TokenStream {
             fn order(&self) -> Option<String> { self.order.clone() }
             fn hidden(&self) -> bool { self.hidden }
             fn setting_type(&self) -> ModSettingType { self.setting_type.clone() }
+        }
+    };
+    gen.into()
+}
+
+fn impl_prototype_base_macro(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl PrototypeBase for #name {
+            fn localised_name(&self) -> Option<LocalisedString> { self.localised_name.clone() }
+            fn localised_description(&self) -> Option<LocalisedString> { self.localised_description.clone() }
+            fn order(&self) -> String { self.order.clone() }
         }
     };
     gen.into()
