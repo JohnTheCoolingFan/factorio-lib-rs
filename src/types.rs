12c9557a772290d7f6aa026130f4dc8492955b04
JohnTheCoolingFan <ivan8215145640@gmail.com>
2021-08-09 22:24:08 +0300

    AutoplaceSpecification
    With a bit of completely broken code on top

--- src/types.rs
+++ src/types.rs
@@ -1794,3 +1794,87 @@ pub struct CameraEffectTriggerEffectItem {
     max_distance: u16, // Default: 0
     strength: f32, // Default: 0
 }
+
+#[derive(Debug)]
+pub struct AutoplaceSpecification {
+    control: String, // Default: "" // id of autoplace control
+    default_enabled: bool, // Default: true
+    force: String, // Default: "neutral"
+    order: String, // Default: ""
+    placement_density: u32, // Default: 1
+    tile_restriction: Vec<String>, // Default: empty // Official docs are not clear about what this actually is, assuming it's a list of String
+    base: AutoplaceSpecificationBase,
+}
+
+#[derive(Debug)]
+pub enum AutoplaceSpecificationBase {
+    Expression(ExpressionBasedAutoplaceSpecification),
+    Peak(PeakBasedAutoplaceSpecification)
+}
+
+#[derive(Debug)]
+pub struct ExpressionBasedAutoplaceSpecification {
+    probability_expression: NoiseExpression,
+    richness_expression: NoiseExpression
+}
+
+#[derive(Debug)]
+pub struct PeakBasedAutoplaceSpecification {
+    sharpness: f64, // Default: 0
+    max_probability: f64, // Default: 1
+    richness_base: f64, // Default: 0
+    richness_multiplier: f64, // Default: 0
+    richness_multiplier_distance_bonus: f64, // Default: 0
+    random_probability_penalty: f64, // Default: 0
+    peaks: Vec<AutoplacePeak>, // If not specified, interpret specification as peak
+    coverage: f64, // Default: calculated from existing peaks // What
+    starting_area_amount: u32, // Default: 0
+    starting_area_size: f64, // Default: 10
+}
+
+#[derive(Debug)]
+pub struct AutoplacePeak {
+    influence: f64, // Default: 1
+    min_influence: f64, // Default: f64::MIN
+    max_influence: f64, // Default: f64::MAX
+    richness_influence: f64, // Default: 0
+    noise_layer: String, // Default: ""
+    noise_persistence: f64, // Default: 0.5
+    noise_octaves_difference: f64, // Default: 0
+    noise_scale: f64, // Default: 1
+    dimensions: Vec<Dimension> // Default: empty // Only one of each type
+}
+
+#[derive(Debug)]
+pub enum Dimension {
+    StartingAreaWeight(DimensionSpec),
+    Elevation(DimensionSpec),
+    Water(DimensionSpec),
+    Temperature(DimensionSpec),
+    Aux(DimensionSpec),
+    TierFromStart(DimensionSpec),
+    Distance(DimensionSpec),
+}
+
+#[derive(Debug)]
+pub struct DimensionSpec {
+    optimal: f64,
+    range: f64, // Default: 0
+    max_range: f64, // Default: range * 1.5 // Default value taken from Factorio base mod source code, version 1.1.37, decoratives.lua, lines 11-17
+    top_property_limit: f64, // Default: f64::MAX // Seems to be unused
+}
+
+#[derive(Debug)]
+pub enum NoiseExpression {
+    Variable(String), // variable_name
+    FunctionApplication(String, String), // function_name and arguments //  FIXME // This does not actually satisfy the api, because arguments make my brain explode
+    LiteralBoolean(bool), // literal_value
+    LiteralNumber(f32), // literal_value
+    LiteralString(String), // literal_value
+    LiteralObject(String), // FIXME // I'm not going to implement this properly.
+    LiteralExpression(Box<NoiseExpression>), // literal_value // oh god no
+    ArrayConstruction(Vec<Box<NoiseExpression>>), // FIXME // Not implemented properly
+    ProcedureDelimeter(Box<NoiseExpression>), // expression
+    IfElseChain(String), // FIXME // no
+}
+
