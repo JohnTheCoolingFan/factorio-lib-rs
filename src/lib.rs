10874e3dba8f8e7495f12788f86f962fc356e12b
JohnTheCoolingFan <ivan8215145640@gmail.com>
2021-08-11 03:28:37 +0300

    Added test that checks PrototypeGeneral size and prints it
    This is done to monitor the memory usage and to help determine which
    prototypes use the most memory to box them, so smaller prototypes don't
    use insane amount of memeory

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,15 @@ pub mod prototypes;
 pub mod prototype_type;
 pub mod types;
 
+#[cfg(test)]
+use crate::prototypes::PrototypeGeneral;
+
 // I guess this is a good place for todos
 
 // TODO: FromLua for prototypes
 // TODO: rest of the prototypes
+
+#[test]
+fn check_prototypes_size() {
+    println!("Size of PrototypeGeneral enum: {} bytes", std::mem::size_of::<PrototypeGeneral>())
+}
