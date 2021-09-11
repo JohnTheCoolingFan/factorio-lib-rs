3a100ee7dbac9a361beb2097c8f83c51a75b6ed8
JohnTheCoolingFan <ivan8215145640@gmail.com>
2021-09-11 12:10:45 +0300

    Removed PrototypeGeneral

--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,7 @@ pub mod prototypes;
 pub mod prototype_type;
 pub mod types;
 
-#[cfg(test)]
-use crate::prototypes::PrototypeGeneral;
-
 // I guess this is a good place for todos
 
 // TODO: FromLua for prototypes
 // TODO: rest of the prototypes
-
-#[test]
-fn check_prototypes_size() {
-    println!("Size of PrototypeGeneral enum: {} bytes", std::mem::size_of::<PrototypeGeneral>())
-}
