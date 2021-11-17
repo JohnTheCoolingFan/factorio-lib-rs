7adaad25c5a5bfbc48017063f66faeef37bb2131
JohnTheCoolingFan <ivan8215145640@gmail.com>
2021-11-17 19:27:59 +0300

    PrototypeFromLua impls

--- src/types/sound.rs
+++ src/types/sound.rs
@@ -1,33 +1,48 @@
+use crate::PrototypeFromLua;
+use factorio_lib_rs_derive::PrototypeFromLua;
+use mlua::{prelude::LuaResult, Lua, Value};
+use crate::{DataTable, ResourceRecord, ResourceType};
+use mlua::FromLua;
+
 use crate::types::FileName;
 
 /// <https://wiki.factorio.com/Types/LayeredSound>
 pub type LayeredSound = Vec<Sound>; // `layers`
 
 /// <https://wiki.factorio.com/Types/Sound>
-#[derive(Debug)]
+#[derive(Debug, PrototypeFromLua)]
 pub struct Sound {
     aggregation: Option<SoundAggregation>,
-    allow_random_repeat: Option<bool>,
-    audible_distance_modifier: Option<f64>,
+    #[default(false)]
+    allow_random_repeat: bool,
+    #[default(1.0)]
+    audible_distance_modifier: f64,
+    #[use_self_if_not_found]
+    #[prototype]
     variations: Vec<SoundVariation> // If variations table not present, use the same table, but construct single variation.
 }
 
 /// <https://wiki.factorio.com/Types/Sound#aggregation>
-#[derive(Debug)]
+#[derive(Debug, PrototypeFromLua)]
 pub struct SoundAggregation {
     max_count: u32,
-    progress_threshold: Option<f32>,
+    #[default(1.0)]
+    progress_threshold: f32,
     remove: bool,
-    count_already_playing: Option<bool>
+    #[default(false)]
+    count_already_playing: bool
 }
 
 /// <https://wiki.factorio.com/Types/Sound#variations>
-#[derive(Debug)]
+#[derive(Debug, PrototypeFromLua)]
 pub struct SoundVariation {
+    #[resource]
     filename: FileName,
-    volume: Option<f32>,
+    #[default(1.0)]
+    volume: f32,
     preload: Option<bool>,
-    speed: Option<f32>,
+    #[default(1.0)]
+    speed: f32,
     min_speed: Option<f32>, // >= 1/64, Ignored if speed is present
     max_speed: Option<f32>  // Mandatory if min_speed is present, >= min_speed
 }
