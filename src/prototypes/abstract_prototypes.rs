5d186ba22caa408cd63e2e45a3ded2f1ad1d6b71
JohnTheCoolingFan <ivan8215145640@gmail.com>
2021-12-17 16:52:04 +0300

    Remove previous Abstract prototypes an their imports

--- src/prototypes/abstract_prototypes.rs
+++ src/prototypes/abstract_prototypes.rs
@@ -1,25 +1,14 @@
 //! This module contains abstract prototypes used for [PrototypeReference](crate::PrototypeReference).
 //! Not all properties are implemented
 
-use crate::prototypes::{Prototype, PrototypeBase, PrototypeBaseSpec};
-use crate::concepts::LocalisedString;
+use crate::prototypes::Prototype;
 
 // TODO: Finish other abstract prototypes
 // TODO: impl DataTableAccessable
 
-/// <https://lua-api.factorio.com/next/LuaAchievementPrototype.html>
-#[derive(Debug, Prototype, PrototypeBase)]
-pub struct AbstractAchievementPrototype {
+/// <https://wiki.factorio.com/PrototypeBase>
+#[derive(Debug, Clone, Prototype)]
+pub struct AbstractPrototypeBase {
     name: String,
-    prototype_base: PrototypeBaseSpec,
-    allowed_without_fight: bool,
-    hidden: bool,
-    valid: bool,
-}
-
-/// <https://lua-api.factorio.com/next/LuaEntityPrototype.html>
-#[derive(Debug, Prototype, PrototypeBase)]
-pub struct AbstractEntityPrototype {
-    name: String,
-    prototype_base: PrototypeBaseSpec,
+    // TODO (not important): type property
 }
