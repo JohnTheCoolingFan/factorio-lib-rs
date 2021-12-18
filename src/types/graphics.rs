fef1f12205ad5dba3e80498a6fe802b34b044d51
JohnTheCoolingFan <ivan8215145640@gmail.com>
2021-12-19 02:00:19 +0300

    SpriteSize converted to tuple struct

--- src/types/graphics.rs
+++ src/types/graphics.rs
@@ -13,7 +13,8 @@ pub type AnimationFrameSequence = Vec<u16>;
 /// <https://wiki.factorio.com/Types/Sprite#position>
 pub type SpritePosition = (i16, i16);
 /// Width and Height <https://wiki.factorio.com/Types/Sprite#width>
-pub type SpriteSize = (i16, i16);
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct SpriteSize(i16, i16);
 /// <https://wiki.factorio.com/Types/SpriteSizeType>
 pub type SpriteSizeType = i16;
 
