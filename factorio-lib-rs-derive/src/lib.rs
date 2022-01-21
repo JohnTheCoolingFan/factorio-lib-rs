38bfe2a9d574087668bc237ad2e35332d5b68729
JohnTheCoolingFan <ivan8215145640@gmail.com>
2022-01-22 00:57:18 +0300

    Entity with owner and health for RollingStock

--- factorio-lib-rs-derive/src/lib.rs
+++ factorio-lib-rs-derive/src/lib.rs
@@ -150,6 +150,8 @@ pub fn rolling_stock_macro_derive(input: TokenStream) -> TokenStream {
     let ast = syn::parse(input).unwrap();
     let mut ts = impl_rolling_stock_macro(&ast);
     ts.extend(impl_vehicle_macro(&ast));
+    ts.extend(impl_entity_with_owner_macro(&ast));
+    ts.extend(impl_entity_with_health_macro(&ast));
     ts.extend(impl_entity_macro(&ast));
     ts.extend(impl_prototype_base_macro(&ast));
     ts
