fed81747bdfd27d70275fe33a6e668daea65cb6d
JohnTheCoolingFan <ivan8215145640@gmail.com>
2022-04-02 15:06:00 +0300

    Renamed new_lua_instance to new_factorio_lua

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,7 @@ use mlua::{Lua, prelude::LuaResult, Integer, Table};
 /// Unfinished wrapper around [mlua::Lua::new] that sets some global variables
 /// Adds `table_size` global function into environment
 #[cfg(feature = "lua")]
-pub fn new_lua_instance() -> LuaResult<Lua> {
+pub fn new_factorio_lua() -> LuaResult<Lua> {
     let lua = Lua::new();
 
     {
