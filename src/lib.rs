d84f5ae4d4a8f31bb7562209e92616141aa0d0d1
JohnTheCoolingFan <ivan8215145640@gmail.com>
2022-04-10 19:49:52 +0300

    Paragraphs

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,7 @@ use mlua::{Lua, prelude::LuaResult, Integer, Table};
 
 // TODO: add more features
 /// Unfinished wrapper around [mlua::Lua::new] that sets some global variables
+///
 /// Adds `table_size` global function into environment
 #[cfg(feature = "lua")]
 pub fn new_factorio_lua() -> LuaResult<Lua> {
