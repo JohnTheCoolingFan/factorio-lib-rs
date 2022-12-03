d8573836df118f0ca736929e9accba8d93b4a9fd
JohnTheCoolingFan <ivan8215145640@gmail.com>
2022-12-03 23:04:42 +0300

    New abstract_prototype_get macro
    My first declarative macro! Wooo!

--- src/prototypes/abstract_prototypes.rs
+++ src/prototypes/abstract_prototypes.rs
@@ -1,5 +1,4 @@
 use crate::prototypes::{DataTable, DataTableAccessable, Prototype, PrototypeType, PrototypesErr};
-use factorio_lib_rs_derive::abstract_prototype_get;
 
 #[derive(Debug, Clone)]
 pub struct AbstractEntity {
@@ -17,14 +16,24 @@ impl Prototype for AbstractEntity {
     }
 }
 
+macro_rules! abstract_prototype_get {(
+    $data_table:ident, $name:ident,
+    $( $ptype:ident $(,)? )*
+    ) => (
+    $( if let Some(prototype) = $data_table.$ptype.get($name) {
+        return Ok(Self{name: $name.into(), prototype_type: prototype.prototype_type()})
+    } )*
+    return Err(PrototypesErr::PrototypeNotFound($name.into()))
+)}
+
 impl DataTableAccessable for AbstractEntity {
     fn find<'a>(_data_table: &'a DataTable, _name: &str) -> Result<&'a Self, PrototypesErr> {
         Err(PrototypesErr::AbstractFind)
     }
 
     fn find_cloned(data_table: &DataTable, name: &str) -> Result<Self, PrototypesErr> {
-        // TODO
-        abstract_prototype_get!(arrow, artillery_flare)
+        // TODO: add other types
+        abstract_prototype_get!(data_table, name, arrow, artillery_flare);
     }
 
     fn extend(self, _data_table: &mut DataTable) -> Result<(), PrototypesErr> {
