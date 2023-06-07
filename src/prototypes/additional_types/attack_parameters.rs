5db8e476cc597d61c7a8c7e41ac760926af2e2e5
JohnTheCoolingFan <ivan8215145640@gmail.com>
2023-06-07 09:39:57 +0300

    Ported additional_types::attack_parameters to serde

--- src/prototypes/additional_types/attack_parameters.rs
+++ src/prototypes/additional_types/attack_parameters.rs
@@ -4,15 +4,15 @@ use super::{
     AmmoType, CircularParticleCreationSpecification, CircularProjectileCreationSpecification,
     Factorio2DVector, RangeMode,
 };
-use crate::prototypes::{GetPrototype, PrototypeFromLua};
-use factorio_lib_rs_derive::prot_from_str;
-use strum_macros::{AsRefStr, EnumDiscriminants, EnumString};
+use crate::util::defaults::*;
+use serde::Deserialize;
+use strum_macros::{AsRefStr, EnumString};
 
 // ========== // AttackParameters // ========== //
 
 /// <https://wiki.factorio.com/Types/AttackParameters>
-#[derive(Debug, Clone, EnumDiscriminants)]
-#[strum_discriminants(derive(EnumString), strum(serialize_all = "kebab-case"))]
+#[derive(Debug, Clone, Deserialize)]
+#[serde(tag = "type", rename_all = "kebab-case")]
 pub enum AttackParameters {
     // Depends on `type` key
     Projectile(ProjectileAttackParameters), // "projectile"
@@ -20,184 +20,123 @@ pub enum AttackParameters {
     Stream(StreamAttackParameters),         // "stream"
 }
 
-impl<'lua> PrototypeFromLua<'lua> for AttackParameters {
-    fn prototype_from_lua(
-        value: mlua::Value<'lua>,
-        lua: &'lua mlua::Lua,
-        data_table: &mut crate::prototypes::DataTable,
-    ) -> mlua::Result<Self> {
-        if let mlua::Value::Table(table) = &value {
-            Ok(
-                match table
-                    .get::<_, String>("type")?
-                    .parse()
-                    .map_err(mlua::Error::external)?
-                {
-                    AttackParametersDiscriminants::Projectile => AttackParameters::Projectile(
-                        ProjectileAttackParameters::prototype_from_lua(value, lua, data_table)?,
-                    ),
-                    AttackParametersDiscriminants::Beam => AttackParameters::Beam(
-                        BeamAttackParameters::prototype_from_lua(value, lua, data_table)?,
-                    ),
-                    AttackParametersDiscriminants::Stream => AttackParameters::Stream(
-                        StreamAttackParameters::prototype_from_lua(value, lua, data_table)?,
-                    ),
-                },
-            )
-        } else {
-            Err(mlua::Error::FromLuaConversionError {
-                from: value.type_name(),
-                to: "AttackParameters",
-                message: Some("expected table".into()),
-            })
-        }
-    }
-}
-
-/// <https://wiki.factorio.com/Types/ProjectileAttackParameters>
-#[derive(Debug, Clone, PrototypeFromLua)]
-pub struct ProjectileAttackParameters {
-    pub range: f32,
-    pub cooldown: f32,
-    #[default(0_f32)]
+#[derive(Debug, Clone, Deserialize)]
+pub struct BaseAttackParameters {
+    #[serde(flatten)]
+    pub range: AttackParametersRange,
+    pub cooldown: AttackParametersCooldown,
+    #[serde(default)]
     pub min_range: f32, // Default: 0
-    #[default(1_f32)]
+    #[serde(default = "default_from_i8::<f32, 1>")]
     pub turn_range: f32, // Default: 1
-    #[default(0_f32)]
+    #[serde(default)]
     pub fire_penalty: f32, // Default: 0
-    #[default(0_f32)]
+    #[serde(default)]
     pub rotate_penalty: f32, // Default: 0
-    #[default(0_f32)]
+    #[serde(default)]
     pub health_penalty: f32, // Default: 0
-    #[default(RangeMode::CenterToCenter)]
+    #[serde(default = "default_rangemode_center_to_center")]
     pub range_mode: RangeMode, // Default: "center-to-center"
-    #[default(range)]
-    pub min_attack_distance: f32, // Default: `range`
-    #[default(1_f32)]
+    #[serde(default = "default_from_i8::<f32, 1>")]
     pub damage_modifier: f32, // Default: 1
-    #[default(1_f32)]
+    #[serde(default = "default_from_i8::<f32, 1>")]
     pub ammo_consumption_modifier: f32, // Default: 1
-    #[default(0_f32)]
+    #[serde(default)]
     pub cooldown_deviation: f32, // Default: 0
-    #[default(0_u32)]
+    #[serde(default)]
     pub warmup: u32, // Default: 0
-    #[default(0_f32)]
+    #[serde(default)]
     pub lead_target_for_projectile_speed: f32, // Default: 0
-    #[default(cooldown)]
-    pub movement_slow_down_cooldown: f32, // Default: `cooldown`
-    #[default(1_f64)]
+    #[serde(default = "default_from_i8::<f32, 1>")]
     pub movement_slow_down_factor: f64, // Default: 1
-    pub ammo_type: Option<AmmoType>,
-    #[default(ActivationType::Shoot)]
+    #[serde(default = "default_activation_type_shoot")]
     pub activation_type: ActivationType, // Default: "shoot"
     pub sound: Option<LayeredSound>,
     pub animation: Option<RotatedAnimation>,
     pub cyclic_sound: Option<CyclicSound>,
-    #[default(false)]
+    #[serde(default)]
     pub use_shooter_direction: bool, // Default: false
-    pub ammo_categories: Option<Vec<String>>, // (Names) Name of AmmoCategory
-    #[default(Factorio2DVector(0.0, 0.0))]
-    pub prohectile_center: Factorio2DVector, // Default: (0, 0)
-    #[default(0_f32)]
+    #[serde(flatten)]
+    pub ammo_categories: AmmoCategories,
+}
+
+#[derive(Debug, Clone, Deserialize)]
+#[serde(untagged)]
+pub enum AttackParametersRange {
+    Both {
+        range: f32,
+        min_attack_distance: f32,
+    },
+    Single {
+        range: f32,
+    },
+}
+
+#[derive(Debug, Clone, Deserialize)]
+#[serde(untagged)]
+pub enum AttackParametersCooldown {
+    Both {
+        cooldown: f32,
+        movement_slow_down_cooldown: f32,
+    },
+    Single {
+        cooldown: f32,
+    },
+}
+
+const fn default_rangemode_center_to_center() -> RangeMode {
+    RangeMode::CenterToCenter
+}
+
+const fn default_activation_type_shoot() -> ActivationType {
+    ActivationType::Shoot
+}
+
+#[derive(Debug, Clone, Deserialize)]
+#[serde(untagged)]
+pub enum AmmoCategories {
+    Many {
+        ammo_type: AmmoType,
+        ammo_categories: Vec<String>, // Names of AmmoCategory
+    },
+    Single {
+        ammo_category: String,
+    },
+}
+
+/// <https://wiki.factorio.com/Types/ProjectileAttackParameters>
+#[derive(Debug, Clone, Deserialize)]
+pub struct ProjectileAttackParameters {
+    #[serde(flatten)]
+    pub base: BaseAttackParameters,
+    #[serde(default)]
+    pub projectile_center: Factorio2DVector, // Default: (0, 0)
+    #[serde(default)]
     pub projectile_creation_distance: f32, // Default: 0
     pub shell_particle: Option<CircularParticleCreationSpecification>,
     pub projectile_creation_parameters: Option<CircularProjectileCreationSpecification>,
-    #[default(0_f32)]
+    #[serde(default)]
     pub projectile_orientation_offset: f32, // Default: 0
 }
 
 /// <https://wiki.factorio.com/Types/BeamAttackParameters>
-#[derive(Debug, Clone, PrototypeFromLua)]
+#[derive(Debug, Clone, Deserialize)]
 pub struct BeamAttackParameters {
-    pub range: f32,
-    pub cooldown: f32,
-    #[default(0_f32)]
-    pub min_range: f32, // Default: 0
-    #[default(1_f32)]
-    pub turn_range: f32, // Default: 1
-    #[default(0_f32)]
-    pub fire_penalty: f32, // Default: 0
-    #[default(0_f32)]
-    pub rotate_penalty: f32, // Default: 0
-    #[default(0_f32)]
-    pub health_penalty: f32, // Default: 0
-    #[default(RangeMode::CenterToCenter)]
-    pub range_mode: RangeMode, // Default: "center-to-center"
-    #[default(range)]
-    pub min_attack_distance: f32, // Default: `range`
-    #[default(1_f32)]
-    pub damage_modifier: f32, // Default: 1
-    #[default(1_f32)]
-    pub ammo_consumption_modifier: f32, // Default: 1
-    #[default(0_f32)]
-    pub cooldown_deviation: f32, // Default: 0
-    #[default(0_u32)]
-    pub warmup: u32, // Default: 0
-    #[default(0_f32)]
-    pub lead_target_for_projectile_speed: f32, // Default: 0
-    #[default(cooldown)]
-    pub movement_slow_down_cooldown: f32, // Default: `cooldown`
-    #[default(1_f64)]
-    pub movement_slow_down_factor: f64, // Default: 1
-    pub ammo_type: Option<AmmoType>,
-    #[default(ActivationType::Shoot)]
-    pub activation_type: ActivationType, // Default: "shoot"
-    pub sound: Option<LayeredSound>,
-    pub animation: Option<RotatedAnimation>,
-    pub cyclic_sound: Option<CyclicSound>,
-    #[default(false)]
-    pub use_shooter_direction: bool, // Default: false
-    pub ammo_categories: Option<Vec<String>>, // (Names) Name of AmmoCategory
-    #[default(0_u32)]
+    #[serde(flatten)]
+    pub base: BaseAttackParameters,
+    #[serde(default)]
     pub source_direction_count: u32, // Default: 0
     pub source_offset: Option<Factorio2DVector>,
 }
 
 /// <https://wiki.factorio.com/Types/StreamAttackParameters>
-#[derive(Debug, Clone, PrototypeFromLua)]
+#[derive(Debug, Clone, Deserialize)]
 pub struct StreamAttackParameters {
-    pub range: f32,
-    pub cooldown: f32,
-    #[default(0_f32)]
-    pub min_range: f32, // Default: 0
-    #[default(1_f32)]
-    pub turn_range: f32, // Default: 1
-    #[default(0_f32)]
-    pub fire_penalty: f32, // Default: 0
-    #[default(0_f32)]
-    pub rotate_penalty: f32, // Default: 0
-    #[default(0_f32)]
-    pub health_penalty: f32, // Default: 0
-    #[default(RangeMode::CenterToCenter)]
-    pub range_mode: RangeMode, // Default: "center-to-center"
-    #[default(range)]
-    pub min_attack_distance: f32, // Default: `range`
-    #[default(1_f32)]
-    pub damage_modifier: f32, // Default: 1
-    #[default(1_f32)]
-    pub ammo_consumption_modifier: f32, // Default: 1
-    #[default(0_f32)]
-    pub cooldown_deviation: f32, // Default: 0
-    #[default(0_u32)]
-    pub warmup: u32, // Default: 0
-    #[default(0_f32)]
-    pub lead_target_for_projectile_speed: f32, // Default: 0
-    #[default(cooldown)]
-    pub movement_slow_down_cooldown: f32, // Default: `cooldown`
-    #[default(1_f64)]
-    pub movement_slow_down_factor: f64, // Default: 1
-    pub ammo_type: Option<AmmoType>,
-    #[default(ActivationType::Shoot)]
-    pub activation_type: ActivationType,
-    pub sound: Option<LayeredSound>,
-    pub animation: Option<RotatedAnimation>,
-    pub cyclic_sound: Option<CyclicSound>,
-    #[default(false)]
-    pub use_shooter_direction: bool, // Default: false
-    pub ammo_categories: Option<Vec<String>>, // (Names) Name of AmmoCategory
-    #[default(0_f32)]
+    #[serde(flatten)]
+    base: BaseAttackParameters,
+    #[serde(default)]
     pub fluid_consumption: f32, // Default: 0
-    #[default(0_f32)]
+    #[serde(default)]
     pub gun_barrel_length: f32, // Default: 0
     pub projectile_creation_parameters: Option<CircularProjectileCreationSpecification>,
     pub gun_center_shift: Option<GunCenterShift>,
@@ -207,8 +146,9 @@ pub struct StreamAttackParameters {
 // =============== // Other // ================ //
 
 /// <https://wiki.factorio.com/Types/BaseAttackParameters#activation_type>
-#[derive(Debug, Clone, Eq, PartialEq, Copy, EnumString, AsRefStr)]
+#[derive(Debug, Clone, Eq, PartialEq, Copy, EnumString, AsRefStr, Deserialize)]
 #[strum(serialize_all = "kebab-case")]
+#[serde(rename_all = "kebab-case")]
 pub enum ActivationType {
     Shoot,
     Throw,
@@ -216,58 +156,24 @@ pub enum ActivationType {
     Activate,
 }
 
-prot_from_str!(ActivationType);
-
 /// <https://wiki.factorio.com/Types/StreamAttackParameters#gun_center_shift>
-#[derive(Debug, Clone)]
-pub struct GunCenterShift {
-    pub north: Factorio2DVector,
-    pub east: Factorio2DVector,
-    pub south: Factorio2DVector,
-    pub west: Factorio2DVector,
-}
-
-impl<'lua> PrototypeFromLua<'lua> for GunCenterShift {
-    fn prototype_from_lua(
-        value: mlua::Value<'lua>,
-        lua: &'lua mlua::Lua,
-        _data_table: &mut crate::prototypes::DataTable,
-    ) -> mlua::Result<Self> {
-        if let mlua::Value::Table(t) = &value {
-            if let Some(north) = t.get::<_, Option<Factorio2DVector>>("north")? {
-                let east = t.get::<_, Factorio2DVector>("east")?;
-                let south = t.get::<_, Factorio2DVector>("south")?;
-                let west = t.get::<_, Factorio2DVector>("west")?;
-                Ok(Self {
-                    north,
-                    east,
-                    south,
-                    west,
-                })
-            } else {
-                let vector = lua.unpack::<Factorio2DVector>(value)?;
-                Ok(Self {
-                    north: vector,
-                    east: vector,
-                    south: vector,
-                    west: vector,
-                })
-            }
-        } else {
-            Err(mlua::Error::FromLuaConversionError {
-                from: value.type_name(),
-                to: "GunCenterShift",
-                message: Some("Expected table".into()),
-            })
-        }
-    }
+#[derive(Debug, Clone, Deserialize)]
+#[serde(untagged)]
+pub enum GunCenterShift {
+    Directions {
+        north: Factorio2DVector,
+        east: Factorio2DVector,
+        south: Factorio2DVector,
+        west: Factorio2DVector,
+    },
+    Single(Factorio2DVector),
 }
 
 /// <https://wiki.factorio.com/Types/StreamAttackParameters#fluids>
-#[derive(Debug, Clone, PrototypeFromLua)]
+#[derive(Debug, Clone, Deserialize)]
 pub struct StreamAttackFluid {
-    #[rename("type")]
+    #[serde(rename = "type")]
     pub fluid: String, // Name of Fluid
-    #[default(1_f64)]
+    #[serde(default = "default_from_i8::<f64, 1>")]
     pub damage_modifier: f64, // Default: 1
 }
